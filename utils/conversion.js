a1b835ac5dc217a5ecdcb5b1c47bd74a169ec984
Glitch (lime-balsam-order) <none>
2022-08-22 18:37:42 +0000

    ⛳️🦁 Checkpoint ./utils/conversion.js:46550433/1105

--- utils/conversion.js
+++ utils/conversion.js
@@ -1,3 +1,8 @@
+/*
+This is a utility file that holds methods for dealing with various calculations in the app
+such as working with weather codes, 
+
+*/
 // Weather Codes Hash Map
 let weathercodes = new Map();
 
@@ -19,7 +24,39 @@ const beaufort = {
     else if(windSpeed >= 1 && windSpeed <= 6) {
       return 1;
     }
-    e
+    else if(windSpeed >= 7 && windSpeed <= 11) {
+      return 2;
+    }
+    else if(windSpeed >=12 && windSpeed <=19) {
+      return 3;
+    }
+    else if(windSpeed >=20 && windSpeed <=29) {
+      return 4;
+    }
+    else if(windSpeed >=30 && windSpeed <=39) {
+      return 5;
+    }
+    else if(windSpeed >=40 && windSpeed <=50) {
+      return 6;
+    }
+    else if(windSpeed >=51 && windSpeed <=62) {
+      return 7;
+    }
+    else if(windSpeed >=63 && windSpeed <=75) {
+      return 8;
+    }
+    else if(windSpeed >=76 && windSpeed <=87) {
+      return 9;
+    }
+    else if(windSpeed >=88 && windSpeed <=102) {
+      return 10;
+    }
+    else if(windSpeed >=103 && windSpeed <=117) {
+      return 11;
+    }
+    else if(windSpeed >= 117) {
+      return 12;
+    }
   }
 }
 
