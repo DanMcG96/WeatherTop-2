307f4b072d5fe54db5189b497478b07b6b2498e0
Glitch (lime-balsam-order) <none>
2022-08-22 14:06:29 +0000

    ⛺️👬 Checkpoint ./views/dashboard.hbs:46550433/33 ./views/partials/readings.hbs:46550433/106 ./controllers/dashboard.js:46550433/199

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,11 +2,23 @@
 
 const logger = require("../utils/logger");
 
+const station = {
+  reading: [
+    {
+      code: '100',
+      temp: '10',
+      windSpeed: '15',
+      pressure: '1000',
+    }
+  ]
+}
+
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     const viewData = {
       title: "Template 1 Dashboard",
+      station: reading
     };
     response.render("dashboard", viewData);
   },
