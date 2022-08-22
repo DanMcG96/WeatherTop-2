1c89ffb4e577f9fce33e369c27b9e9cd3a740133
Glitch (lime-balsam-order) <none>
2022-08-22 18:14:36 +0000

    👶🍏 Checkpoint ./utils/conversion.js:46550433/378

--- utils/conversion.js
+++ utils/conversion.js
@@ -1,2 +1,12 @@
 let weathercodes = new Map();
 
+weathercodes.set(100,"Clear");
+weathercodes.set(200, "Partial Clouds");
+weathercodes.set(300, "Cloudy");
+weathercodes.set(400, "Light Showers");
+weathercodes.set(500, "Heavy Showers");
+weathercodes.set(600, "Rain");
+weathercodes.set(700, "Snow");
+weathercodes.set(800, "Thunder");
+
+module.exports = weathercodes;
