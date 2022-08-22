3e72f43a885858f79da9d2adc5f98ebc7ee93d1a
Glitch (lime-balsam-order) <none>
2022-08-22 19:48:38 +0000

    ⛽️🍕 Checkpoint ./utils/conversion.js:46550433/1704

--- utils/conversion.js
+++ utils/conversion.js
@@ -66,18 +66,67 @@ const conversions = {
       return 12;
     }
   },
+  
   // Celsius to Farenheit
   celsiusToFarenheit(temp) {
     return (temp * 1.8) + 32;
   },
+  
+  // Wind Chill Calculator
   windChill(temp, windSpeed) {
     return (13.12 + 0.6215(temp) - 11.37(windSpeed^0.16) + 0.3965(temp)(windSpeed^0.16));
   },
+  
+  //Wind Compass Calculator
   windCompass(windDirection) {
     if(windDirection <=348.75  && windDirection >= 11.25) {
        return "North" ;
        }
-    else if(windDirection >= && windDirection <= )
+    else if(windDirection >= 11.26 && windDirection <= 33.75 ) {
+        return "North North East" ;    
+       }
+    else if(windDirection >= 33.76 && windDirection <= 56.25 ) {
+        return "North East" ;    
+       }
+    else if(windDirection >= 56.26 && windDirection <= 78.75) {
+        return "East North East" ;    
+       }
+    else if(windDirection >= 78.76 && windDirection <= 101.25) {
+        return "East" ;    
+       }
+    else if(windDirection >= 101.26 && windDirection <= 123.75) {
+        return "East South East" ;    
+       }
+    else if(windDirection >= 123.76 && windDirection <= 146.25) {
+        return "South East" ;    
+       }
+    else if(windDirection >= 146.26 && windDirection <= 168.75) {
+        return "South South East" ;    
+       }
+    else if(windDirection >= 168.76 && windDirection <= 191.25) {
+        return "South" ;    
+       }
+    else if(windDirection >= 191.26 && windDirection <= 213.75) {
+        return "South South West" ;    
+       }
+    else if(windDirection >= 213.75 && windDirection <= 236.25) {
+        return "South West" ;    
+       }
+    else if(windDirection >= 236.26 && windDirection <= ) {
+        return "" ;    
+       }
+    else if(windDirection >= && windDirection <= ) {
+        return "" ;    
+       }
+    else if(windDirection >= && windDirection <= ) {
+        return "" ;    
+       }
+    else if(windDirection >= && windDirection <= ) {
+        return "" ;    
+       }
+    else if(windDirection >= && windDirection <= ) {
+        return "" ;    
+       }
   }
    
 }
